5b5d8d75459958abfda6e6abe51cb45765a10db9
TakuyaHarayama <harayama@ijgn.jp>
2016-06-15 23:36:27 +0900

    Add spec to user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name' do
+    context 'when blank' do
+      user = User.new
+      it { expect(user).to be_invalid }
+    end
+    context 'when present' do
+      user = User.new(name: "hoge")
+      it { expect(user).to be_valid }
+    end
+    context 'when length is 51' do
+      user = User.new(name: "123456789012345678901234567890123456789012345678901")
+      it { expect(user).to be_invalid }
+    end
+  end
 end
